797995a6cf7f9352b7f978aa8f97a3c56c93cf22
daoh329 <daoh329@gmail.com>
2023-08-21 14:32:47 +0700

    Start

---
+++ server/app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Định nghĩa các tuyến (routes)
+app.get('/', (req, res) => {
+  res.send('Chào mừng đến với máy chủ Node.js!');
+});
+
+app.get('/about', (req, res) => {
+  res.send('Đây là trang giới thiệu về chúng tôi.');
+});
+
+// Khởi chạy máy chủ
+app.listen(port, () => {
+  console.log(`Máy chủ đang lắng nghe tại cổng ${port}`);
+});
