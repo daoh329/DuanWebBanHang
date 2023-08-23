8863b0b913c1446bd4d1e36a10c49ae590ae6f8e
MinhPK02434 <yuukidmon@gmail.com>
2023-08-23 13:15:01 +0700

    Update code

--- client/src/views/App.js
+++ client/src/views/App.js
@@ -1,12 +1,26 @@
-import logo from './logo.svg';
-
+//App.js
+import React from 'react';
+import { BrowserRouter, Routes, Route, } from 'react-router-dom';
+import '../views/App.scss';
+import Home from './Home/Home';
+import Nav from './Nav/Nav'
+import Footer from './Footer/Footer';
+import CardProduct from '../components/CardProduct';
 import Detail from '../components/detail';
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-<Detail/>
-      </header>
+    <div className='App'>
+      <BrowserRouter>
+        <Nav />
+        <header>
+          <Routes>
+          <Route path="/home" element={<Home />} />
+          <Route path="/card" element={<CardProduct />} />
+          <Route path="/detail" element={<Detail />} />
+          </Routes>
+        </header>
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 };
