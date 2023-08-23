a2cbdbd164e0fa6542cf40161f18ae3877519311
trantai102 <100552532+trantai102@users.noreply.github.com>
2023-08-23 09:43:06 +0700

    home

--- client/src/views/App.js
+++ client/src/views/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+//App.js
+import React from 'react';
+import { BrowserRouter, Routes, Route, } from 'react-router-dom';
+import '../views/App.scss';
+import Home from './Home/Home';
+import Nav from './Nav/Nav'
+import Footer from './Footer/Footer';
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <BrowserRouter>
+        <Nav />
+        <header>
+          <Routes>
+          <Route path="/home" element={<Home />} />
+          </Routes>
+        </header>
+        <Footer />
+      </BrowserRouter>
     </div>
   );
-}
+};
 
 export default App;
---
+++ client/src/views/Nav/Nav.js
@@ -0,0 +1,77 @@
+import React, { useState } from 'react';
+import { Layout, Menu, Input, Badge, Avatar, Dropdown, Affix } from 'antd';
+import { DownOutlined, BellOutlined, ShoppingCartOutlined, UserOutlined, SearchOutlined, TagOutlined, EnvironmentOutlined, CommentOutlined, PhoneOutlined } from '@ant-design/icons';
+
+const { Header } = Layout;
+
+const App = () => {
+    const [menuOpenKeys, setMenuOpenKeys] = useState([]);
+
+    const handleMenuOpenChange = (openKeys) => {
+        setMenuOpenKeys(openKeys);
+    };
+
+    const menu = (
+        <Menu>
+            <Menu.Item key="1">Đăng nhập</Menu.Item>
+            <Menu.Item key="2">Đăng xuất</Menu.Item>
+        </Menu>
+    );
+
+    return (
+        <Layout>
+            <Affix offsetTop={0}>
+                <div className="header" style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' , background:'#f8f8fc',margintop:'0px'}}>
+                    <a style={{ marginRight: '20px', color: '#333', textDecoration: 'none', display: 'flex', alignItems: 'center' }}><TagOutlined style={{ marginRight: '8px' }} /> Khuyến mãi</a>
+                    <a style={{ marginRight: '20px', color: '#333', textDecoration: 'none', display: 'flex', alignItems: 'center' }}><EnvironmentOutlined style={{ marginRight: '8px' }} /> Hệ thống showroom</a>
+                    <a style={{ marginRight: '20px', color: '#333', textDecoration: 'none', display: 'flex', alignItems: 'center' }}><CommentOutlined style={{ marginRight: '8px' }} /> Tư vẫn doanh nghiệp</a>
+                    <a style={{ marginRight: '20px', color: '#333', textDecoration: 'none', display: 'flex', alignItems: 'center' }}><PhoneOutlined style={{ marginRight: '8px' }} /> Liên hệ</a>
+                </div>
+
+                <Header className="header" style={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between', backgroundColor: 'black' }}>
+                    <div className="logo" style={{ width: '80px', marginRight: '16px', color: '#ffffff' }}>
+                        {/* Add your logo here */}
+                        <span>Logo</span>
+                    </div>
+                    <Menu
+                        theme="dark"
+                        mode="horizontal"
+                        defaultSelectedKeys={['1']}
+                        openKeys={menuOpenKeys}
+                        onOpenChange={handleMenuOpenChange}
+                        style={{ width: '120px', }}
+                    >
+                        <Menu.SubMenu
+                            key="sub1"
+                            icon={<DownOutlined />}
+                            title={<span>Danh mục</span>}
+                            popupClassName="white-menu"
+                            style={{ background: "black" }}
+                        >
+                            <Menu.Item key="1">Danh mục 1</Menu.Item>
+                            <Menu.Item key="2">Danh mục 2</Menu.Item>
+                            <Menu.Item key="3">Danh mục 3</Menu.Item>
+                            <Menu.Item key="4">Danh mục 4</Menu.Item>
+                        </Menu.SubMenu>
+                    </Menu>
+                    <div className="search-container" style={{ flex: 1, display: 'flex', justifyContent: 'center', }}>
+                        <Input.Search placeholder="Tìm kiếm" style={{ width: '600px', color: '#ffffff', }} />
+                    </div>
+                    <div className="right-icons" style={{ display: 'flex', alignItems: 'center' }}>
+                        <Badge count={5} style={{ marginTop: '10px', marginRight: '10px', backgroundColor: '#f50', color: '#fff' }}>
+                            <BellOutlined style={{ fontSize: '24px', color: '#ffffff', margin: '10px' }} />
+                        </Badge>
+                        <Badge count={3} style={{ marginRight: '10px', marginTop: '10px' }}>
+                            <ShoppingCartOutlined style={{ fontSize: '24px', color: '#ffffff', margin: '10px' }} />
+                        </Badge>
+                        <Dropdown overlay={menu} trigger={['click']}>
+                            <Avatar icon={<UserOutlined />} style={{ backgroundColor: '#005c42', margin: '10px' }} />
+                        </Dropdown>
+                    </div>
+                </Header>
+            </Affix>
+        </Layout>
+    );
+};
+
+export default App;
