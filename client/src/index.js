3ac0f0e540b7c223b9ee87cab5dedf988cd2e48b
trantai102 <100552532+trantai102@users.noreply.github.com>
2023-08-24 11:04:01 +0700

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -9,8 +9,6 @@ import reportWebVitals from './reportWebVitals';
 // thư viện mdb
 import 'mdb-react-ui-kit/dist/css/mdb.min.css';
 import '@fortawesome/fontawesome-free/css/all.min.css';
-import 'bootstrap-css-only/css/bootstrap.min.css';
-import 'mdbreact/dist/css/mdb.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
