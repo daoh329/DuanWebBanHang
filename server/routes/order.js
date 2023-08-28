56780f43247155f7384a06fcffe4f857bd8d6f6e
trantai102 <100552532+trantai102@users.noreply.github.com>
2023-08-28 17:14:40 +0700

    update codoe

--- server/routes/order.js
+++ server/routes/order.js
@@ -9,8 +9,8 @@ router.post('/order', OrderController.order);
 // Trong routes.js hoặc tệp tương tự
 router.get('/json', OrderController.json);
 
-router.put(`/order/confirm/:id`, OrderController.confirmOrder);
-router.put(`/order/cancel/:id`, OrderController.cancelOrder);
+router.put(`/confirm/:id`, OrderController.confirmOrder);
+router.put(`/cancel/:id`, OrderController.cancelOrder);
 
 
 module.exports = router;
