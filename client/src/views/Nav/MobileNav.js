a0d4480420368ed7c3e0d97198890f9e93d6d232
MinhPK02434 <yuukidmon@gmail.com>
2023-10-06 10:17:35 +0700

    update /noi-dung

--- client/src/views/Nav/MobileNav.js
+++ client/src/views/Nav/MobileNav.js
@@ -9,7 +9,7 @@ const MobileNav = () => {
                 <a href="/" className="nav-item is-active" active-color="orange">
                     <i className="fa fa-house"></i>
                 </a>
-                <a href="#" className="nav-item" active-color="#0d6efd">
+                <a href="/noi-dung" className="nav-item" active-color="#0d6efd">
                     <i className="fa-brands fa-microsoft"></i>
                 </a>
                 <a href="/cart" className="nav-item" active-color="blue">
