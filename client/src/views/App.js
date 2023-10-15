0e652a0ed5492b8a6dc6d705cfdc90a93c5d492d
Nguyễn Mạnh Hùng <114969627+danhcuvip@users.noreply.github.com>
2023-10-15 12:35:24 +0700

    Update App.js
    Fix lỗi không vào được trang login

--- client/src/views/App.js
+++ client/src/views/App.js
@@ -54,13 +54,14 @@ const App = () => {
       }
     } catch (e) {
       console.log(e);
-      localStorage.removeItem("user", JSON.stringify(user));
+      localStorage.removeItem("user");
     }
   };
 
   useEffect(() => {
     getUser();
   }, []);
+  console.log(user);
 
   return (
     <div className="App">
