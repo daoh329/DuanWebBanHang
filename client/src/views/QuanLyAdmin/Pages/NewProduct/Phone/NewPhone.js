27e0f6b8022179665969b40a369800d7277612ef
Nguyễn Mạnh Hùng <114969627+danhcuvip@users.noreply.github.com>
2023-11-08 15:26:22 +0700

    Hung
    add capacity in form add phone

--- client/src/views/QuanLyAdmin/Pages/NewProduct/Phone/NewPhone.js
+++ client/src/views/QuanLyAdmin/Pages/NewProduct/Phone/NewPhone.js
@@ -6,7 +6,19 @@ import axios from "axios";
 import { CKEditor } from "@ckeditor/ckeditor5-react";
 import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
 import ImageInput from "../ImageComponent/ImageInput";
-import { Modal, Select, Space, Spin, message, notification } from "antd";
+import {
+  Button,
+  Form,
+  InputNumber,
+  Modal,
+  Select,
+  Space,
+  Spin,
+  message,
+  notification,
+} from "antd";
+import { PlusOutlined } from "@ant-design/icons";
+import { formatCapacity } from "../../../../../util/formatCapacity";
 
 const { Option } = Select;
 
@@ -232,8 +244,97 @@ function NewPhone() {
     setCapacitySubmit(updatedRomInfo);
   };
 
+  // function open modal
+  const [isOpenModalCapcity, setIsOpenModalCapcity] = useState(false);
+
+  const handleCancel = () => {
+    setIsOpenModalCapcity(false);
+  };
+
+  const openModalAddCapacity = () => {
+    setIsOpenModalCapcity(true);
+  };
+
+  // function logic modal
+  // modal capacity
+  const [isLoading, setIsLoading] = useState(false);
+  const onFinish = async (values) => {
+    setIsLoading(true);
+    try {
+      const result = await axios.post(
+        `${process.env.REACT_APP_API_URL}/List/add/capacity`,
+        values
+      );
+
+      if (result.status === 200) {
+        setTimeout(() => {
+          setIsLoading(false);
+          notification.success({
+            message: "Cập nhật thành công!",
+          });
+        }, 2000);
+        getCapacity();
+        setIsOpenModalCapcity(false);
+      } else {
+        setTimeout(() => {
+          setIsLoading(false);
+          notification.error({
+            message: "Cập nhật thất bại!",
+          });
+        }, 2000);
+      }
+    } catch (error) {
+      console.log(error);
+      setTimeout(() => {
+        setIsLoading(false);
+        notification.error({
+          message: "Cập nhật thất bại!",
+        });
+      }, 2000);
+    }
+  };
+
+  const onFinishFailed = (errorInfo) => {
+    console.log(errorInfo);
+    // message.error("Thêm thất bại");
+  };
+
   return (
     <div className="container-content">
+      <Modal
+        title="Thêm lựa chọn cho dung lượng"
+        open={isOpenModalCapcity}
+        onCancel={handleCancel}
+        footer={false}
+      >
+        <Form
+          onFinish={onFinish}
+          onFinishFailed={onFinishFailed}
+          autoComplete="off"
+        >
+          <Form.Item
+            name={"capacity"}
+            rules={[
+              {
+                required: true,
+                message: "Vui lòng nhập dữ liệu rồi tiếp tục!",
+              },
+            ]}
+          >
+            <InputNumber
+              placeholder="Nhập dung lượng"
+              type="number"
+              min={0}
+              style={{ borderRadius: "3px", width: "100%" }}
+            />
+          </Form.Item>
+          <Form.Item wrapperCol={{ offset: 19, span: 4 }}>
+            <Button loading={isLoading} type="primary" htmlType="submit">
+              Xác nhận
+            </Button>
+          </Form.Item>
+        </Form>
+      </Modal>
       <form
         className="form"
         id="form-create-laptop"
@@ -399,7 +500,13 @@ function NewPhone() {
                 <tbody>
                   {capacitySubmit.map((rom, index) => (
                     <tr key={index}>
-                      <td>
+                      <td
+                        style={{
+                          display: "flex",
+                          justifyContent: "space-between",
+                          alignItems: "center",
+                        }}
+                      >
                         <select
                           className="form-control"
                           type="text"
@@ -413,12 +520,15 @@ function NewPhone() {
                           </option>
                           {[...capacity].map((capacity, index) => (
                             <option value={capacity.capacity}>
-                              {capacity.capacity === 1024
-                                ? "1TB"
-                                : capacity.capacity + "GB"}
+                              {formatCapacity(capacity.capacity)}
                             </option>
                           ))}
                         </select>
+                        <Button
+                          onClick={openModalAddCapacity}
+                          icon={<PlusOutlined />}
+                          style={{ margin: "0 0 0 5px" }}
+                        />
                       </td>
                       <td>
                         <input
